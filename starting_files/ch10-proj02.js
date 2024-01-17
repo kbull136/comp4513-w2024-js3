25ce0a92941598a3f78286dc099ea6d9b5410ad1
Keen Bullecer <kbull136@mtroyal.ca>
2024-01-17 14:21:19 -0700

    started working on stuff

--- starting_files/ch10-proj02.js
+++ starting_files/ch10-proj02.js
@@ -1,10 +1,10 @@
-
+import {Play} from "./play-module.js";
 
 document.addEventListener("DOMContentLoaded", function() {
 
 	
 	const url = 'https://www.randyconnolly.com//funwebdev/3rd/api/shakespeare/play.php';
-
+   let play;
    /*
      To get a specific play, add play name via query string, 
 	   e.g., url = url + '?name=hamlet';
@@ -20,4 +20,38 @@ document.addEventListener("DOMContentLoaded", function() {
        Some possibilities: if using Visual Code, use Live Server extension; if Brackets,
        use built-in Live Preview.
     */
+
+   //Play Selector
+   document.querySelector('#playList').addEventListener('change', (e) => {
+      console.log(e.target.value);
+
+      let playURL = url + "?name=" + e.target.value;
+      
+      play = new Play (fetchPlay(playURL));
+
+      //display Play Content
+   });
+
+   //Act Selector
+   document.querySelector('#actList').addEventListener('change', (e) => {
+
+   });
+   
+   //Scene Selector
+   this.documentElement.querySelector('#sceneList').addEventListener('change', (e) => {
+
+   });
+
+
+   // fetch play info
+   async function fetchPlay(url) {
+
+      try {
+         const response = await fetch(url);
+         const data = await response.json();
+         return data;
+      } catch (err) {
+         console.log(err);
+      }
+   }
 });
--- starting_files/play-module.js
+++ starting_files/play-module.js
@@ -1,2 +1,80 @@
 /* In this module, create three classes: Play, Act, and Scene. */
 
+class Play {
+    constructor(fetchedPlay){
+        this.acts = [];
+        this.title = fetchedPlay.title;
+        this.short = fetchedPlay.short;
+        this.persona = fetchedPlay.persona;
+
+        fetchedPlay.acts.forEach( act => {
+            this.acts.push(new Act(act));
+        })
+    }
+
+    displayFirstAct() {
+
+    }
+}
+
+class Act {
+    constructor(act){
+        this.name = act.name;
+        this.scenes = [];
+
+        act.scenes.forEach( scn => {
+            this.scenes.push(new Scene(scn));
+        });
+    }
+
+    displayFirstScene() {
+        const sceneDiv = document.querySelector('#sceneHere');
+        const sceneName = document.querySelector('#sceneHere h4');
+        const sceneTitle = document.querySelector("#sceneHere .title");
+        const stageDir = document.querySelector('.direction');
+        //const scene
+
+        this.scenes.forEach( (scene) => {
+            if(scene.name == "SCENE I") {
+                sceneName.textContent = scene.name;
+                sceneTitle.textContent = scene.title;
+                stageDir.textContent = scene.stageDirection;
+
+                scene.speeches.forEach( speech => {
+                    sceneDiv.append(createSpeech(speech.speaker, speech.lines));
+                });
+
+            }
+        });
+    }
+}
+
+class Scene {
+    constructor(scene){
+        this.speeches = scene.speeches;
+        this.stageDirection = scene.stageDirection;
+        this.title = scene.title;
+        this.name = scene.name;
+    }
+
+    createSpeech(speaker, lines) {
+        const div = document.createElement('div');
+        const span = document.createElement('span').textContent = speaker;
+        
+        div.setAttribute('class', "speech");
+        div.append(span);
+
+        lines.forEach(line => {
+            const p = document.createElement('p').textContent = line;
+            div.append(p);
+        });
+
+
+
+
+        return div;
+    }
+
+}
+
+export {Play}
